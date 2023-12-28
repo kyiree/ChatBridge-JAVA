939ec18fce61da9dc43da5890690f50ba08d8273
kyiree <851294066@qq.com>
2023-12-29 00:47:02 +0800

    init

--- src/test/java/com/cn/chat/bridge/CryptUtilsTest.java
+++ src/test/java/com/cn/chat/bridge/CryptUtilsTest.java
@@ -9,21 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class CryptUtilsTest {
 
     public static void main(String[] args) {
-        SM4 sm4 = new SM4(Base64.decode("1tDZ7omDsiHpVtqGp7Aoiw=="));
-
-
-        String mailPwd = sm4.encryptBase64("opjytfrblmbxbdhc");
-        String openPlusKey = sm4.encryptBase64("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");
-        String adminEmail = sm4.encryptBase64("851294066@qq.com");
-        String adminPwd = sm4.encryptBase64("123456789");
-        String mailUsername = sm4.encryptBase64("851294066@qq.com");
-        String weChatSecret = sm4.encryptBase64("3542fe00c37e2e5f71d1985139a764cf");
-        System.out.println("mailPwd=" + mailPwd);
-        System.out.println("openPlusKey=" + openPlusKey);
-        System.out.println("adminEmail=" + adminEmail);
-        System.out.println("adminPwd=" + adminPwd);
-        System.out.println("mailUsername=" + mailUsername);
-        System.out.println("weChatSecret=" + weChatSecret);
 
 
 
