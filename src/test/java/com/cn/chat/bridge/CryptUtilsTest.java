48296cf5fa59a9baebc2de78472b0b8fc470ed03
kyiree <851294066@qq.com>
2023-12-29 00:48:57 +0800

    init

--- src/test/java/com/cn/chat/bridge/CryptUtilsTest.java
+++ src/test/java/com/cn/chat/bridge/CryptUtilsTest.java
@@ -1,8 +1,5 @@
 package com.cn.chat.bridge;
 
-import cn.hutool.core.codec.Base64;
-import cn.hutool.core.util.RandomUtil;
-import cn.hutool.crypto.symmetric.SM4;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
