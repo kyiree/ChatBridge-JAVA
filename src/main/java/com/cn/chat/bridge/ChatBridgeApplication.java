923c4770195274e5649d51f699703970a1e9b91c
kyiree <851294066@qq.com>
2024-01-24 22:22:28 +0800

    feat(tt): 提交信息

--- src/main/java/com/cn/chat/bridge/ChatBridgeApplication.java
+++ src/main/java/com/cn/chat/bridge/ChatBridgeApplication.java
@@ -14,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ChatBridgeApplication {
 
     public static void main(String[] args) {
+        //
         SpringApplication.run(ChatBridgeApplication.class, args);
     }
 
